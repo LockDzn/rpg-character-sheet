275c529702b75988f3d3f2eced305b4ac95e27ce
Ryan Souza <ryanssouza00@gmail.com>
2021-01-24 19:21:07 -0300

    🎉 some modifications and new features

--- characterSheet.js
+++ characterSheet.js
@@ -1,202 +1,245 @@
-$(document).ready(function(){
+const data = {
+    name: "Claudio",
+    player: "Ryan",
+    occupation: "Caçador",
+    age: 21,
+    sex: "male",
+    birthplace: "São paulo",
+    residence: "São paulo",
 
-    const data = {
-        name: "Claudio",
-        player: "Ryan",
-        occupation: "Caçador",
-        age: 21,
-        sex: "male",
-        birthplace: "São paulo",
-        residence: "São paulo",
+    life: {
+        current: 12,
+        max: 12,
+    },
+    sanity: {
+        current: 62,
+        max: 62,
+    },
 
-        life: {
-            current: 11,
-            max: 11,
+    weapons: [
+        {
+            name: "Balestra",
+            type: "Arco",
+            damage: "1d20",
+            numCurrent: 1,
+            numMax: 1,
+            attack: 5,
+            reach: "10 m",
+            defect: 1,
+            area: "",
         },
-        sanity: {
-            current: 32,
-            max: 40,
+        {
+            name: "Canivete",
+            type: "Briga",
+            damage: "1d10",
+            numCurrent: "",
+            numMax: "",
+            attack: "1/2",
+            reach: "",
+            defect: 1,
+            area: "",
         },
+    ],
+}
 
-        weapons: [
-            {
-                name: "Balestra",
-                type: "Arco",
-                damege: "1d20",
-                numCurrent: 1,
-                numMax: 1,
-                attack: 5,
-                reach: "10 m",
-                defect: 1,
-                area: "",
-            },
-            {
-                name: "Canivite",
-                type: "Briga",
-                damege: "1d10",
-                numCurrent: "",
-                numMax: "",
-                attack: "1/2",
-                reach: "",
-                defect: 1,
-                area: "",
-            },
-        ],
+data.weapons.map((weapon, index) => {
+    addWeaponToTable(weapon, index);
+});
+
+
+$("#name").val(data.name);
+$("#player").val(data.player);
+$("#occupation").val(data.occupation);
+$("#age").val(data.age);
+$("#sex").val(data.sex);
+$("#birthplace").val(data.birthplace);
+$("#residence").val(data.residence);
+
+$(".lifeBar").css("width", `${calculateBar(data.life.current, data.life.max)}%`);
+$("#lifeCount").text(`${data.life.current}/${data.life.max}`)
+$("#lifeCurrent").val(data.life.current)
+$("#lifeMax").val(data.life.max)
+
+$(".sanityBar").css("width", `${calculateBar(data.sanity.current, data.sanity.max)}%`);
+$("#sanityCount").text(`${data.sanity.current}/${data.sanity.max}`)
+$("#sanityCurrent").val(data.sanity.current)
+$("#sanityMax").val(data.sanity.max)
+
+
+const diceModal = $("#diceAttributes");
+const lifeModal = $("#lifeModal");
+const sanityModal = $("#sanityModal");
+
+$(window).click(function(event) {
+    if (event.target.id == "diceAttributes") {
+        diceModal.css("display", "none");
+        $("#diceNumber").text("");
+        $("#diceType").text("");
+
+        $(".modalDice").css("transform", "rotate(0deg)");
+        $(".modalDice").css("-webkit-transform", "rotate(0deg)");
+    }else if (event.target.id == "lifeModal") {
+        lifeModal.css("display", "none");
+    }else if (event.target.id == "sanityModal") {
+        sanityModal.css("display", "none");
+    }else if (event.target.id == "addWeaponModal") {
+        closeModal("#addWeaponModal");
     }
+});
 
-    data.weapons.map((weapon, index) => {
-        addWeaponToTable(weapon, index);
-    });
+function rollAtribute(atribute){
 
+    console.log(this)
 
-    $("#name").val(data.name);
-    $("#player").val(data.player);
-    $("#occupation").val(data.occupation);
-    $("#age").val(data.age);
-    $("#sex").val(data.sex);
-    $("#birthplace").val(data.birthplace);
-    $("#residence").val(data.residence);
+    diceModal.css("display", "block");
 
-    $(".lifeBar").css("width", `${calculateBar(data.life.current, data.life.max)}%`);
-    $(".sanityBar").css("width", `${calculateBar(data.sanity.current, data.sanity.max)}%`);
+    setTimeout(() => {
+        $(".modalDice").css("transform", "rotate(360deg)");
+        $(".modalDice").css("-webkit-transform", "rotate(360deg)");
+    }, 1000);
 
+    setTimeout(() => {
 
-    const diceModal = $("#diceAttributes");
-    const lifeModal = $("#lifeModal");
-    const sanityModal = $("#sanityModal");
+        const diceNumber = rollDice("1d20")
+        const diceType = calcDice(11, diceNumber)
+        $("#diceNumber").text(diceNumber)
+        $("#diceType").text(diceType)
 
-    $(window).click(function(event) {
-        if (event.target.id == "diceAttributes") {
+        setTimeout(() => {
             diceModal.css("display", "none");
             $("#diceNumber").text("");
             $("#diceType").text("");
 
             $(".modalDice").css("transform", "rotate(0deg)");
             $(".modalDice").css("-webkit-transform", "rotate(0deg)");
-        }else if (event.target.id == "lifeModal") {
-            lifeModal.css("display", "none");
-        }else if (event.target.id == "sanityModal") {
-            sanityModal.css("display", "none");
-        }else if (event.target.id == "addWeaponModal") {
-            closeModal("#addWeaponModal");
-        }
-    });
+        }, 20000);
+    }, 2000);
+}
 
-    $("#btn").click(function(){
+$(".lifeBar").click(function(){
 
-        console.log(this)
+    console.log(this);
+    lifeModal.css("display", "block");
+});
 
-        diceModal.css("display", "block");
+$(".sanityBar").click(function(){
 
-        setTimeout(() => {
-            $(".modalDice").css("transform", "rotate(360deg)");
-            $(".modalDice").css("-webkit-transform", "rotate(360deg)");
-        }, 1000);
+    console.log(this);
+    sanityModal.css("display", "block");
+});
 
-        setTimeout(() => {
+$("#addWeapon").click(function(){
+    openModal("#addWeaponModal");
+});
 
-            var diceNumber = Math.floor(Math.random() * 21);
-            $("#diceNumber").text(diceNumber);
-            $("#diceType").text("BOM");
+$('#lesion').change(function() {
+    if(this.checked) {
+        console.log('Modo lesionamento grave ativado!');
+    }else {
+        console.log('Modo lesionamento grave desativado!');
+    } 
+});
 
-            setTimeout(() => {
-                diceModal.css("display", "none");
-                $("#diceNumber").text("");
-                $("#diceType").text("");
+$('#injury').change(function() {
+    if(this.checked) {
+        console.log('Modo lesionamento ativado!');
+    }else {
+        console.log('Modo lesionado desativado!');
+    } 
+});
 
-                $(".modalDice").css("transform", "rotate(0deg)");
-                $(".modalDice").css("-webkit-transform", "rotate(0deg)");
-            }, 20000);
-        }, 2000);
-    });
+$('#dying').change(function() {
+    if(this.checked) {
+        console.log('Modo morrendo ativado!');
+    }else {
+        console.log('Modo morrendo desativado!');
+    } 
+});
 
-    $(".lifeBar").click(function(){
+$('#traumatized').change(function() {
+    if(this.checked) {
+        console.log('Modo traumatizado ativado!');
+    }else {
+        console.log('Modo traumatizado desativado!');
+    } 
+});
 
-        console.log(this);
-        lifeModal.css("display", "block");
-    });
+$('#crazed').change(function() {
+    if(this.checked) {
+        console.log('Modo enlouquecido ativado!');
+    }else {
+        console.log('Modo enlouquecido desativado!');
+    } 
+});
 
-    $(".sanityBar").click(function(){
+$('#addWeaponForm').submit(function( event ) {
 
-        console.log(this);
-        sanityModal.css("display", "block");
-    });
+    var weaponType = "";
 
-    $("#addWeapon").click(function(){
-        openModal("#addWeaponModal");
-    });
+    if($("#weaponType").val() == "fire"){
+        weaponType = "Fogo";
+    }else if($("#weaponType").val() == "arch") {
+        weaponType = "Arco";
+    }else if($("#weaponType").val() == "fight") {
+        weaponType = "Briga";
+    }
 
-    $('#lesion').change(function() {
-        if(this.checked) {
-            console.log('Modo lesionamendo grave ativado!');
-        }else {
-            console.log('Modo lesionamendo grave desativado!');
-        } 
-    });
+    const weapon = {
+        name: $("#weaponName").val(),
+        type: weaponType,
+        damage: $("#weapondamage").val(),
+        numCurrent: $("#weaponNumCurrent").val(),
+        numMax: $("#weaponNumMax").val(),
+        attack:$("#weaponAttack").val(),
+        reach: $("#weaponReach").val(),
+        defect: $("#weaponDefect").val(),
+        area: $("#weaponArea").val(),
+    }
 
-    $('#injury').change(function() {
-        if(this.checked) {
-            console.log('Modo lesionamendo ativado!');
-        }else {
-            console.log('Modo lesionado desativado!');
-        } 
-    });
+    data.weapons.push(weapon);
+    const id = data.weapons.length-1;
+    addWeaponToTable(weapon, id);
 
-    $('#dying').change(function() {
-        if(this.checked) {
-            console.log('Modo morrendo ativado!');
-        }else {
-            console.log('Modo morrendo desativado!');
-        } 
-    });
+    closeModal("#addWeaponModal");
+    event.preventDefault();
+});
 
-    $('#traumatized').change(function() {
-        if(this.checked) {
-            console.log('Modo traumatizado ativado!');
-        }else {
-            console.log('Modo traumatizado desativado!');
-        } 
-    });
+$('#changeLife').submit(function( event ) {
 
-    $('#crazed').change(function() {
-        if(this.checked) {
-            console.log('Modo enlouquecido ativado!');
-        }else {
-            console.log('Modo enlouquecido desativado!');
-        } 
-    });
+    let current = Number($("#lifeCurrent").val())
+    const max = Number($("#lifeMax").val())
 
-    $('#addWeaponForm').submit(function( event ) {
+    if(current > max) {
+        alert('A vida atual não pode ser maior que a maxima!')
+        current = max
+    }
 
-        var weaponType = "";
+    data.life.current = current
+    data.life.max = max
+    $(".lifeBar").css("width", `${calculateBar(current, max)}%`)
+    $("#lifeCount").text(`${current}/${max}`)
 
-        if($("#weaponType").val() == "fire"){
-            weaponType = "Fogo";
-        }else if($("#weaponType").val() == "arch") {
-            weaponType = "Arco";
-        }else if($("#weaponType").val() == "fight") {
-            weaponType = "Briga";
-        }
+    closeModal("#lifeModal");
+    event.preventDefault();
+});
 
-        const weapon = {
-            name: $("#weaponName").val(),
-            type: weaponType,
-            damege: $("#weaponDamege").val(),
-            numCurrent: $("#weaponNumCurrent").val(),
-            numMax: $("#weaponNumMax").val(),
-            attack:$("#weaponAttack").val(),
-            reach: $("#weaponReach").val(),
-            defect: $("#weaponDefect").val(),
-            area: $("#weaponArea").val(),
-        }
+$('#changeSanity').submit(function( event ) {
 
-        data.weapons.push(weapon);
-        const id = data.weapons.length-1;
-        addWeaponToTable(weapon, id);
+    let current = Number($("#sanityCurrent").val())
+    const max = Number($("#sanityMax").val())
 
-        closeModal("#addWeaponModal");
-        event.preventDefault();
-    });
+    if(current > max) {
+        alert('A sanidade atual não pode ser maior que a maxima!')
+        current = max
+    }
+
+    data.sanity.current = current
+    data.sanity.max = max
+    $(".sanityBar").css("width", `${calculateBar(current, max)}%`)
+    $("#sanityCount").text(`${current}/${max}`)
+
+    closeModal("#sanityModal");
+    event.preventDefault();
 });
 
 function calculateBar(current, max){
@@ -209,6 +252,73 @@ function calculateBar(current, max){
         const string = value.toString().split(".")[0];
         const percentage = Number(string);
         return percentage;
+    }
+}
+
+
+function calcDice(ability, dice) {
+    // Não encontrei uma forma mais fácil, então fiz assim
+
+    const table = [
+        { normal: 20 },
+        { normal: 19, good: 20 },
+        { normal: 18, good: 20 },
+        { normal: 17, good: 19 },
+        { normal: 16, good: 19, extreme: 20 },
+        { normal: 15, good: 18, extreme: 20 },
+        { normal: 14, good: 18, extreme: 20 },
+        { normal: 13, good: 17, extreme: 20 },
+        { normal: 12, good: 17, extreme: 20 },
+        { normal: 11, good: 16, extreme: 20 },
+        { normal: 10, good: 16, extreme: 19 },
+        { normal: 9, good: 16, extreme: 19 },
+        { normal: 8, good: 15, extreme: 19 },
+        { normal: 7, good: 14, extreme: 19 },
+        { normal: 6, good: 14, extreme: 18 },
+        { normal: 5, good: 13, extreme: 18 },
+        { normal: 4, good: 13, extreme: 18 },
+        { normal: 3, good: 12, extreme: 18 },
+        { normal: 2, good: 12, extreme: 18 },
+        { normal: 1, good: 11, extreme: 17 },
+    ]
+
+    const type = table[ability-1]
+
+    if(type.extreme) {
+
+        if(dice >= type.extreme) return 'Extremo'
+        if(dice >= type.good) return 'Sucesso Bom'
+        if(dice >= type.normal) return 'Sucesso Normal'
+        if(dice <= type.normal) return "Fracasso"
+
+    } else if(type.good) {
+        if(dice >= type.good) return 'Sucesso Bom'
+        if(dice >= type.normal) return 'Sucesso Normal'
+        if(dice <= type.normal) return "Fracasso"
+    } else if(type.normal) {
+        if(dice >= type.normal) return 'Sucesso Normal'
+        if(dice <= type.normal) return "Fracasso"
+    }
+
+}
+
+function rollDice(dice) {
+
+    let [count, max] = dice.split('d')
+
+    if(Number(count) && Number(max)) {
+        count = Number(count)
+        max = Number(max)
+
+        let total = 0
+
+        for (let i = 0; i < count; i++) {
+            total += Math.floor(Math.random() * max+1)
+        }
+
+        return total
+    }else {
+        return null
     }
 }
 
@@ -231,7 +341,7 @@ function addWeaponToTable(weapon, id) {
             ${weapon.name}
         </td>
         <td>${weapon.type}</td>
-        <td>${weapon.damege}</td>
+        <td>${weapon.damage}</td>
         <td>${weapon.numCurrent}</td>
         <td>${weapon.numMax}</td>
         <td>${weapon.attack}</td>
